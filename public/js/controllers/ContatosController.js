da32a251bfee9507b08b7b8b31784ef218ff2dcf
Romildo Paiter <romildopaiter@gmail.com>
2015-01-22 00:21:04 -0200

    Adicionando o ng-repeat

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -4,4 +4,21 @@ angular.module('contatooh').controller('ContatosController',
   	$scope.incrementa = function(){
   		$scope.total ++;
   	};
+	$scope.contatos = [
+		{
+			"_id": 1,
+			"nome": "Contato angular ",
+			"email": "email1@email.com"
+		},
+		{
+			"_id": 2,
+			"nome": "Contato angular 2",
+			"email": "email2@email.com"
+		},
+		{
+			"_id": 3,
+			"nome": "Contato angular 3",
+			"email": "email3@email.com"
+		}
+	];
 });
