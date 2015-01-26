092b6e50e33609104ed5fbcc23768dbe50e4528d
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-01-26 12:53:33 -0300

    Refatoracao da forma de exibir o conteudo da grid de contatos

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,26 +1,16 @@
 angular.module('contatooh').controller( 'ContatosController' ,
   function($scope, $http) {
-  	$scope.total = 0;
+  	
   	$scope.filtro = "";
-  	$scope.incrementa = function(){
-  		$scope.total ++;
-  	};
-	$scope.contatos = [
-		{
-			"_id": 1,
-			"nome": "Contato angular ",
-			"email": "email1@email.com"
-		},
-		{
-			"_id": 2,
-			"nome": "Contato angular 2",
-			"email": "email2@email.com"
-		},
-		{
-			"_id": 3,
-			"nome": "Contato angular 3",
-			"email": "email3@email.com"
-		}
-	];
+  	$scope.contatos = [];
+  	
+  	$http.get('/contatos')
+  	.success(function(data){
+  		$scope.contatos = data;
+  	})
+  	.error(function(statusText){
+  		console.log("Não foi possível obter a lista de contatos");
+  		console.log("statusText");
+  	});
   }
 );
