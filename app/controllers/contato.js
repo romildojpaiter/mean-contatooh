be42231a16c19bdfd21c0ae9d3f1cfd981716363
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-02-15 01:05:56 -0300

    7.15 A funcao populate e busca de referencias

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     var Contato = app.models.contato;
     
 	controller.listarContatos = function(req, res) {
-        Contato.find().exec()
+        Contato.find().populate('emergencia').exec()
         .then(
             function(contatos){
                 res.json(contatos);
