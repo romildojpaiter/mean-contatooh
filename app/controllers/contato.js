2521ead8b2a39ad1414e631c38bc1b464c8aa9eb
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-02-15 00:00:01 -0300

    7.6 Utilizando modelos 2

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,10 +1,20 @@
-module.exports = function() {
+module.exports = function(app) {
 
 	var controller = {};
     var Contato = app.models.contato;
     
 	controller.listarContatos = function(req, res) {
-	}
+        Contato.find().exec()
+            .then(
+                function(contatos){
+                    res.json(contatos);
+                },
+                function(erro){
+                    console.error(erro);
+                    res.status(500).json(erro);
+                }
+            );
+	};
 
 	controller.obtemContato = function(req, res){
 	}
