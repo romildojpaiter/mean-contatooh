611b958bc95549fc19c481ddf9b484c6ea30c8f0
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-02-14 23:42:58 -0300

    7.6 Utilizando modelos

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,69 +1,20 @@
-var contatos = [
-	{_id: 1, nome: 'Contato Exemplo 1',
-		email: 'cont1@empresa.com.br'},
-	{_id: 2, nome: 'Contato Exemplo 2',
-		email: 'cont2@empresa.com.br'},
-	{_id: 3, nome: 'Contato Exemplo 3',
-		email: 'cont3@empresa.com.br'}
-];
-
 module.exports = function() {
 
 	var controller = {};
+    var Contato = app.models.contato;
     
-    var ID_CONTATO_INC = 3;
-
 	controller.listarContatos = function(req, res) {
-		res.json(contatos);
 	}
 
 	controller.obtemContato = function(req, res){
-		// console.log(req.params.id);
-		var idContato = req.params.id;
-		var contato = contatos.filter( 
-			function(contato) {
-				//console.log('controller.obtemContato aqui');
-				return contato._id == idContato;
-			})[0];
-		contato ? 
-			res.json(contato) :
-			res.status(404).send('Contato não encontrado');
 	}
 
 	controller.removeContato = function(request, response) {
-		var idContato = request.params.id;
-		contatos = contatos.filter( function(contato) {
-			return contato._id != idContato;
-		});
-		response.send(204).end();
 	}
 
 	controller.salvarContato = function(request, response) {
-		console.log('API: Salvando Contatos: ');
-
-        var contato = request.body;
-        contato = contato._id ?
-                    atualiza(contato) :
-                    adiciona(contato);
-        response.json(contato);
         
 	}
-    
-    function adiciona(contatoNovo){
-        contatoNovo._id = ++ID_CONTATO_INC;
-        contatos.push(contatoNovo);
-        return contatoNovo;
-    }
-    
-    function atualiza(contatoAlterar){
-        contatos = contatos.map(function(contato) {
-            if(contato._id == contatoAlterar._id) {
-                contato = contatoAlterar;
-            }
-            return contato;
-        });
-        return contatoAlterar;                        
-    }
 
 	return controller;
 
