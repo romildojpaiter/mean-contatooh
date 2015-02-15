12ecbb5a0fabd704451759df82e7b57fc1b2f994
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-02-15 00:09:36 -0300

    7.8 Buscando pelo Id

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -5,19 +5,32 @@ module.exports = function(app) {
     
 	controller.listarContatos = function(req, res) {
         Contato.find().exec()
-            .then(
-                function(contatos){
-                    res.json(contatos);
-                },
-                function(erro){
-                    console.error(erro);
-                    res.status(500).json(erro);
-                }
-            );
+        .then(
+            function(contatos){
+                res.json(contatos);
+            },
+            function(erro){
+                console.error(erro);
+                res.status(500).json(erro);
+            }
+        );
 	};
 
 	controller.obtemContato = function(req, res){
-	}
+        var _id = req.params.id;
+        Contato.findById(_id).exec()
+        .then(
+            function(contato) {
+                if (!contato) throw new Error("Contato não encontrado");
+                
+                res.json(contato);
+            },
+            function(erro){
+                console.log(erro);
+                res.status(404).json(erro);
+            }
+        );
+	};
 
 	controller.removeContato = function(request, response) {
 	}
