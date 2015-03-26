49dc2f9f4119ad43c4057f58e7b3bf82278c41c2
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-03-26 13:34:34 -0400

    8.11 Combinando views do servidor/cliente

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
-module.exports = function(app){
-    /*app.get('/', function(req, res) {
+module.exports = function(app) {
+    
+    app.get('/', function(req, res) {
+        console.log('Presente na route index /');
         var login = '';
         if(req.user) {
             login = req.user.login;
         }
+        console.log(login);
         res.render('index', { "usuarioLogado" : login });
-    });*/
+    });
 }
