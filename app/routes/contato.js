9e521a4b9179391a0b010f636b58d9e4aa3bfe3c
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-03-26 12:57:35 -0400

    Nesse momento esta autenticando via github

--- app/routes/contato.js
+++ app/routes/contato.js
@@ -1,14 +1,14 @@
+function verificaAutenticacao(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        res.status('401').json('Não autoriazado');
+    }
+} 
+
 module.exports = function(app) {
     
-    function verificaAutenticacao(req, res, next){
-        if(req.isAuthenticated()){
-            return next();
-        } else {
-            res.status('401').json('Não autoriazado');
-        }
-    }    
-	
-	var controller = app.controllers.contato;
+    var controller = app.controllers.contato;
 
     
 	/*app.route('/contatos')
