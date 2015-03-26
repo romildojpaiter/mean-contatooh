cda418d26f7dd30cedea322c7b0d1502557294e5
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-03-26 08:48:01 -0400

    Alterações ainda realizadas com a versão antiga do Livro

---
+++ app/routes/auth.js
@@ -0,0 +1,23 @@
+var passport = require('passport');
+
+module.exports = function(app) {
+
+    app.get('/auth/github', passport.authenticate('github'));
+    app.get('/auth/github/callback',
+            passport.authenticate('github', {
+                successRedirect: '/'
+                }
+            )
+    );
+    
+    app.get('/', function(req, res, next) {
+        if(req.isAuthenticated()) {
+            // permite que as rotas sejam processadas
+            return next();
+        } else {
+            // renderiza auth.ejs
+            res.render("auth");
+        }
+    });
+    
+}
