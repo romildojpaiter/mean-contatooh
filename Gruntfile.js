62691d6af9826ea4c95b59d9b582af95064f2ec2
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-05-05 08:28:42 -0400

    Adicionando o Grunt com o ng-annotate, e executando operaçoes de build do projeto

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,12 +15,28 @@ module.exports = function(grunt) {
 			dist: {
 				src: 'dist'
 			}
-		}
-	
+		},
+        usemin : {
+            html: 'dist/app/views/**/*.ejs'
+        },
+        ngAnnotate : {
+            scripts: {
+                expand: true,
+                scr: ['dist/public/js/**/*.js']
+            }
+        },        
+        useminPrepare : {
+            options: {
+                root: 'dist/public',
+                dest: 'dist/public'    
+            },
+            html: 'dist/app/views/**/*.ejs'
+        },        
 	});
 
-	grunt.registerTask('default', ['dist']);
+	grunt.registerTask('default', ['dist', 'minifica']);
 	grunt.registerTask('dist', ['clean', 'copy']);
+    grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
@@ -28,5 +44,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-ng-annotate');
 
 }
