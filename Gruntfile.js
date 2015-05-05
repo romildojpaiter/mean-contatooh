735362be07a6d6038797f93bb85cfcdd884eba32
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-05-05 01:17:00 -0400

    Adicionando ferramentas ao grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,23 @@ module.exports = function(grunt) {
 				src: ['**', '!Grantfile.js', '!package.json', '!public/bower.json'],
 				dest: 'dist'
 			},
+		},
+		clean: {
+			dist: {
+				src: 'dist'
+			}
 		}
 	
 	});
 
+	grunt.registerTask('default', ['dist']);
+	grunt.registerTask('dist', ['clean', 'copy']);
+
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-usemin');
 
 }
